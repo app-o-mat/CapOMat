8f1f1ecbd20b4926761a595f1080514d51079faf
Lou Franco <lfranco@greenwave-solutions.com>
2020-01-13 20:06:02 -0500

    Add State Capital Data and generator

--- CapOMat/CapPong/View/CapPongScene.swift
+++ CapOMat/CapPong/View/CapPongScene.swift
@@ -39,12 +39,85 @@ class StateCapitalGenerator: ProblemGenerator {
 
 }
 
+class CapitalToStateGenerator: ProblemGenerator {
+    let maxAnswerLength: Int
+
+    static let data = [
+        "Alabama": "Montgomery",
+        "Alaska": "Juneau",
+        "Arizona": "Phoenix",
+        "Arkansas": "Little Rock",
+        "California": "Sacramento",
+        "Colorado": "Denver",
+        "Connecticut": "Hartford",
+        "Delaware": "Dover",
+        "Florida": "Tallahassee",
+        "Georgia": "Atlanta",
+        "Hawaii": "Honolulu",
+        "Idaho": "Boise",
+        "Illinois": "Springfield",
+        "Indiana": "Indianapolis",
+        "Iowa": "Des Moines",
+        "Kansas": "Topeka",
+        "Kentucky": "Frankfort",
+        "Louisiana": "Baton Rouge",
+        "Maine": "Augusta",
+        "Maryland": "Annapolis",
+        "Massachusetts": "Boston",
+        "Michigan": "Lansing",
+        "Minnesota": "Saint Paul",
+        "Mississippi": "Jackson",
+        "Missouri": "Jefferson City",
+        "Montana": "Helena",
+        "Nebraska": "Lincoln",
+        "Nevada": "Carson City",
+        "New Hampshire": "Concord",
+        "New Jersey": "Trenton",
+        "New Mexico": "Santa Fe",
+        "New York": "Albany",
+        "North Carolina": "Raleigh",
+        "North Dakota": "Bismarck",
+        "Ohio": "Columbus",
+        "Oklahoma": "Oklahoma City",
+        "Oregon": "Salem",
+        "Pennsylvania": "Harrisburg",
+        "Rhode Island": "Providence",
+        "South Carolina": "Columbia",
+        "South Dakota": "Pierre",
+        "Tennessee": "Nashville",
+        "Texas": "Austin",
+        "Utah": "Salt Lake City",
+        "Vermont": "Montpelier",
+        "Virginia": "Richmond",
+        "Washington": "Olympia",
+        "West Virginia": "Charleston",
+        "Wisconsin": "Madison",
+        "Wyoming": "Cheyenne",
+    ]
+
+    init() {
+        // Find the longest answer
+        self.maxAnswerLength = CapitalToStateGenerator.data.reduce(0) { max($0, $1.1.count) }
+    }
+
+    func getNextProblem() -> Problem {
+        let stateAndCap = CapitalToStateGenerator.data.randomElement()!
+        var wrongAnswers = Set<String>(CapitalToStateGenerator.data.keys)
+        wrongAnswers.remove(stateAndCap.0)
+
+        return Problem(question: stateAndCap.1,
+                       answer: stateAndCap.0,
+                       wrongAnswers: wrongAnswers)
+    }
+
+}
+
 class CapPongScene: GameScene {
 
     init(size: CGSize) {
        super.init(size: size, gameLogics:
-       [PongOnePlayerLogic(generator: StateCapitalGenerator(), numButtonLines: 2),
-        PongTwoPlayerLogic(generator: StateCapitalGenerator(), numButtonLines: 2)])
+       [PongOnePlayerLogic(generator: CapitalToStateGenerator(), numButtonLines: 2),
+        PongTwoPlayerLogic(generator: CapitalToStateGenerator(), numButtonLines: 2)])
    }
 
 }
